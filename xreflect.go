2fbe39169a1562bb1074e22b063070c5d85a3d75
morrisxyang <morris.x.yang@gmail.com>
2023-07-13 16:57:06 +0800

    feat: auto create if nil

--- xreflect.go
+++ xreflect.go
@@ -9,24 +9,27 @@ import (
 )
 
 // SetEmbedStructField 设置嵌套的结构体字段, obj 必须是指针
-func SetEmbedStructField(obj interface{}, path string, value interface{}) error {
+func SetEmbedStructField(obj interface{}, fieldPath string, fieldValue interface{}) error {
 	if obj == nil {
 		return errors.New("obj must not be nil")
 	}
-	if path == "" {
-		return errors.New("path must not be empty")
+	if fieldPath == "" {
+		return errors.New("field path must not be empty")
 	}
 	if reflect.TypeOf(obj).Kind() != reflect.Pointer {
 		return errors.New("obj must be pointer")
 	}
 
 	target := reflect.ValueOf(obj).Elem()
-	fieldNames := strings.Split(path, ".")
+	fieldNames := strings.Split(fieldPath, ".")
 	for _, fieldName := range fieldNames {
 		if fieldName == "" {
-			return fmt.Errorf("path:%s is invalid", path)
+			return fmt.Errorf("field path:%s is invalid", fieldPath)
 		}
 		if target.Kind() == reflect.Pointer {
+			if target.IsNil() {
+				target.Set(reflect.New(target.Type().Elem()).Elem().Addr())
+			}
 			target = reflect.ValueOf(target.Interface()).Elem()
 		}
 		if !target.IsValid() || !target.CanSet() || target.Kind() != reflect.Struct {
@@ -36,11 +39,11 @@ func SetEmbedStructField(obj interface{}, path string, value interface{}) error 
 	}
 
 	if !target.IsValid() || !target.CanSet() {
-		return fmt.Errorf("%s cannot be set", path)
+		return fmt.Errorf("%s cannot be set", fieldPath)
 	}
 
 	targetType := target.Type()
-	actualValue := reflect.ValueOf(value)
+	actualValue := reflect.ValueOf(fieldValue)
 	if targetType != actualValue.Type() {
 		actualValue = actualValue.Convert(targetType)
 	}
