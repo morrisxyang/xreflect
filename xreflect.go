cf113eb377db4d21c1b63735adcc5b895032f230
morrisxyang <morris.x.yang@gmail.com>
2023-08-06 14:23:36 +0800

    feat: add GetEmbedStructFieldX methods

--- xreflect.go
+++ xreflect.go
@@ -348,6 +348,74 @@ func GetStructFieldTag(obj interface{}, fieldName, tagKey string) (string, error
 	return structField.Tag.Get(tagKey), nil
 }
 
+// GetEmbedStructField ...
+func GetEmbedStructField(obj interface{}, fieldPath string) (reflect.StructField, error) {
+	var empty reflect.StructField
+	if obj == nil {
+		return empty, errors.New("obj must not be nil")
+	}
+	target := GetType(obj)
+	if !isSupportedKind(target.Kind(), []reflect.Kind{reflect.Struct}) {
+		return empty, errors.New("obj must be struct")
+	}
+	if fieldPath == "" {
+		return empty, errors.New("field path must not be empty")
+	}
+
+	fieldNames := strings.Split(fieldPath, ".")
+	for i, fieldName := range fieldNames {
+		if fieldName == "" {
+			return empty, fmt.Errorf("field path:%s is invalid", fieldPath)
+		}
+		if target.Kind() == reflect.Pointer {
+			target = target.Elem()
+		}
+		if !isSupportedKind(target.Kind(), []reflect.Kind{reflect.Struct}) {
+			return empty, fmt.Errorf("field %s is not struct", target)
+		}
+
+		structField, ok := target.FieldByName(fieldName)
+		if !ok {
+			return empty, fmt.Errorf("no such field: %s", fieldName)
+		}
+		target = structField.Type
+		if i == len(fieldNames)-1 {
+			return structField, nil
+		}
+	}
+	return empty, nil
+}
+
+// GetEmbedStructFieldKind ...
+func GetEmbedStructFieldKind(obj interface{}, fieldPath string) (reflect.Kind, error) {
+	field, err := GetEmbedStructField(obj, fieldPath)
+	if err != nil {
+		return reflect.Invalid, err
+	}
+
+	return field.Type.Kind(), nil
+}
+
+// GetEmbedStructFieldType ...
+func GetEmbedStructFieldType(obj interface{}, fieldPath string) (reflect.Type, error) {
+	field, err := GetEmbedStructField(obj, fieldPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return field.Type, nil
+}
+
+// GetEmbedStructFieldTypeStr ...
+func GetEmbedStructFieldTypeStr(obj interface{}, fieldPath string) (string, error) {
+	field, err := GetEmbedStructField(obj, fieldPath)
+	if err != nil {
+		return "", err
+	}
+
+	return field.Type.String(), nil
+}
+
 // GetType ...
 func GetType(obj interface{}) reflect.Type {
 	if obj == nil {
--- xreflect_test.go
+++ xreflect_test.go
@@ -465,6 +465,28 @@ func TestGetEmbedFieldXMethods(t *testing.T) {
 			name: "City.Town.Int",
 			args: args{
 				obj:  ct,
+				name: "",
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				assert.EqualError(t, err, "field path must not be empty")
+				return false
+			},
+		},
+		{
+			name: ".Town.Int",
+			args: args{
+				obj:  ct,
+				name: ".Town.Int",
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				assert.EqualError(t, err, "field path:.Town.Int is invalid")
+				return false
+			},
+		},
+		{
+			name: "City.Town.Int",
+			args: args{
+				obj:  ct,
 				name: "City.Town.Int",
 			},
 			want:    1,
@@ -668,6 +690,173 @@ func TestGetStructFieldTag(t *testing.T) {
 				return
 			}
 			assert.Equalf(t, tt.want, got, "GetStructFieldTag(%v, %v, %v)", tt.args.obj, tt.args.fieldName, tt.args.tagKey)
+		})
+	}
+}
+
+func TestGetEmbedStructFieldXMethods(t *testing.T) {
+	to := Town{
+		Int:  1,
+		Str:  "Town",
+		Bool: true,
+		Strs: []string{"Str"},
+	}
+	ci := City{
+		PtrTown: &to,
+		Town:    to,
+	}
+	ct := Country{
+		ID:      1,
+		Name:    "Country",
+		City:    ci,
+		PtrCity: &ci,
+	}
+
+	type args struct {
+		obj  interface{}
+		name string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    interface{}
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "Get",
+			args: args{
+				obj:  ct,
+				name: "Name",
+			},
+			want:    "Country",
+			wantErr: assert.NoError,
+		},
+		{
+			name: "No such field",
+			args: args{
+				obj:  ct,
+				name: "Address",
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				assert.EqualError(t, err, "no such field: Address")
+				return false
+			},
+		},
+		{
+			name: "Nil",
+			args: args{
+				obj:  nil,
+				name: "Name",
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				assert.EqualError(t, err, "obj must not be nil")
+				return false
+			},
+		},
+		{
+			name: "Not a struct",
+			args: args{
+				obj:  "test",
+				name: "Name",
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				assert.EqualError(t, err, "obj must be struct")
+				return false
+			},
+		},
+		{
+			name: "City.Town.Int",
+			args: args{
+				obj:  ct,
+				name: "",
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				assert.EqualError(t, err, "field path must not be empty")
+				return false
+			},
+		},
+		{
+			name: ".Town.Int",
+			args: args{
+				obj:  ct,
+				name: ".Town.Int",
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				assert.EqualError(t, err, "field path:.Town.Int is invalid")
+				return false
+			},
+		},
+		{
+			name: "City.Town.Int",
+			args: args{
+				obj:  ct,
+				name: "City.Town.Int",
+			},
+			want:    1,
+			wantErr: assert.NoError,
+		},
+		{
+			name: "City.Town.Int",
+			args: args{
+				obj:  &ct,
+				name: "City.Town.Int",
+			},
+			want:    1,
+			wantErr: assert.NoError,
+		},
+		{
+			name: "PtrCity.PtrTown.Int",
+			args: args{
+				obj:  &ct,
+				name: "PtrCity.PtrTown.Int",
+			},
+			want:    1,
+			wantErr: assert.NoError,
+		},
+		{
+			name: "City.PtrTown.Int",
+			args: args{
+				obj:  &ct,
+				name: "City.PtrTown.Int",
+			},
+			want:    1,
+			wantErr: assert.NoError,
+		},
+		{
+			name: "City.Town.Bool",
+			args: args{
+				obj:  &ct,
+				name: "City.Town.Bool",
+			},
+			want:    true,
+			wantErr: assert.NoError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetEmbedStructFieldKind(tt.args.obj, tt.args.name)
+			if !tt.wantErr(t, err, fmt.Sprintf("GetEmbedStructFieldKind(%v, %v)", tt.args.obj, tt.args.name)) {
+				return
+			}
+			assert.Equalf(t, reflect.TypeOf(tt.want).Kind(), got, "GetEmbedStructFieldKind(%v, %v)", tt.args.obj, tt.args.name)
+		})
+
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetEmbedStructFieldType(tt.args.obj, tt.args.name)
+			if !tt.wantErr(t, err, fmt.Sprintf("GetEmbedStructFieldType(%v, %v)", tt.args.obj, tt.args.name)) {
+				return
+			}
+			assert.Equalf(t, reflect.TypeOf(tt.want), got, "GetEmbedStructFieldType(%v, %v)", tt.args.obj, tt.args.name)
+		})
+
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetEmbedStructFieldTypeStr(tt.args.obj, tt.args.name)
+			if !tt.wantErr(t, err, fmt.Sprintf("GetEmbedStructFieldTypeStr(%v, %v)", tt.args.obj, tt.args.name)) {
+				return
+			}
+			assert.Equalf(t, reflect.TypeOf(tt.want).String(), got, "GetEmbedStructFieldTypeStr(%v, %v)", tt.args.obj, tt.args.name)
+
 		})
 	}
 }
