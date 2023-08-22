f0138fea28e97216396ccfc6b560a4c77f61b5af
morrisxyang <morris.x.yang@gmail.com>
2023-08-22 14:49:02 +0800

    feat: add CallFunc and CallFuncSlice

---
+++ func.go
@@ -0,0 +1,103 @@
+package xreflect
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+// CallFunc ...
+func CallFunc(fn interface{}, args ...interface{}) ([]reflect.Value, error) {
+	if fn == nil {
+		return nil, errors.New("fn must not be nil")
+	}
+
+	typ := Type(fn)
+	val := Value(fn)
+	if !isSupportedKind(val.Kind(), []reflect.Kind{reflect.Func}) {
+		return nil, errors.New("fn must be func")
+	}
+	if !typ.IsVariadic() && len(args) != typ.NumIn() {
+		return nil, fmt.Errorf("fn params num is %d, but got %d", typ.NumIn(), len(args))
+	}
+	if typ.IsVariadic() {
+		if len(args) < typ.NumIn()-1 {
+			return nil, fmt.Errorf("fn params num is %d at least, but got %d", typ.NumIn()-1, len(args))
+		}
+	}
+
+	reflectArgs := make([]reflect.Value, len(args))
+	// var sliceType reflect.Type
+	for i, arg := range args {
+		// If the argument is nil, use zero value
+		if arg == nil {
+			if typ.IsVariadic() && i >= typ.NumIn()-1 {
+				reflectArgs[i] = reflect.New(typ.In(typ.NumIn() - 1).Elem()).Elem()
+			} else {
+				reflectArgs[i] = reflect.New(typ.In(i)).Elem()
+			}
+		} else {
+			reflectArgs[i] = reflect.ValueOf(arg)
+		}
+	}
+
+	var retValues []reflect.Value
+	retValues = val.Call(reflectArgs)
+
+	if len(retValues) > 0 {
+		// 如果函数最后一个返回值为 error 且不为空, 提取
+		if errResult := retValues[len(retValues)-1].Interface(); errResult != nil {
+			if err, ok := errResult.(error); ok {
+				return retValues[0 : len(retValues)-1], err
+			}
+		}
+	}
+
+	return retValues, nil
+}
+
+// CallFuncSlice ...
+func CallFuncSlice(fn interface{}, args ...interface{}) ([]reflect.Value, error) {
+	if fn == nil {
+		return nil, errors.New("fn must not be nil")
+	}
+
+	typ := Type(fn)
+	val := Value(fn)
+	if !isSupportedKind(val.Kind(), []reflect.Kind{reflect.Func}) {
+		return nil, errors.New("fn must be func")
+	}
+	if !typ.IsVariadic() {
+		return nil, errors.New("fn must be variadic")
+	}
+
+	if len(args) != typ.NumIn() {
+		return nil, fmt.Errorf("use reflect.CallSlice, fn params num should be %d, but got %d",
+			typ.NumIn(), len(args))
+	}
+
+	reflectArgs := make([]reflect.Value, len(args))
+	// var sliceType reflect.Type
+	for i, arg := range args {
+		// If the argument is nil, use zero value
+		if arg == nil {
+			reflectArgs[i] = reflect.New(typ.In(i)).Elem()
+		} else {
+			reflectArgs[i] = reflect.ValueOf(arg)
+		}
+	}
+
+	var retValues []reflect.Value
+	retValues = val.CallSlice(reflectArgs)
+
+	if len(retValues) > 0 {
+		// 如果函数最后一个返回值为 error 且不为空, 提取
+		if errResult := retValues[len(retValues)-1].Interface(); errResult != nil {
+			if err, ok := errResult.(error); ok {
+				return retValues[0 : len(retValues)-1], err
+			}
+		}
+	}
+
+	return retValues, nil
+}
---
+++ func_test.go
@@ -0,0 +1,167 @@
+// Package xreflect ...
+package xreflect
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var addFunc = func(a int, b int) int {
+	return a + b
+}
+
+var addReturnErrorFunc = func(a int, b int) (int, error) {
+	return 0, errors.New("add error")
+}
+
+var onlyReturnErrorFunc = func() error {
+	return errors.New("error")
+}
+
+var emptyFunc = func() {
+	fmt.Println("empty")
+	return
+}
+
+var changeMapFunc = func(m map[string]string) {
+	m["1"] = "1"
+}
+
+var interfaceParamFunc = func(a, b interface{}) int {
+	return a.(int) + b.(int)
+}
+
+var sliceParamFunc = func(ss []int) int {
+	var sum int
+	for _, s := range ss {
+		sum += s
+	}
+	return sum
+}
+
+var pointFunc = func(p *int) {
+	*p = 1
+}
+
+var pureVariadicFunc = func(ps ...*int) int {
+	var sum int
+	for _, p := range ps {
+		sum += *p
+	}
+	return sum
+}
+
+var variadicFunc = func(pi *int, ps ...*int) int {
+	var sum int
+	for _, p := range ps {
+		if p != nil {
+			sum += *p
+		}
+	}
+	if pi != nil {
+		return sum + *pi
+	}
+	return sum
+}
+
+func TestExecFunc(t *testing.T) {
+	_, err := CallFunc(nil, nil)
+	assert.EqualError(t, err, "fn must not be nil")
+
+	_, err = CallFunc("", nil)
+	assert.EqualError(t, err, "fn must be func")
+
+	_, err = CallFunc(addFunc, 1)
+	assert.EqualError(t, err, "fn params num is 2, but got 1")
+
+	_, err = CallFunc(addFunc, 1, 2, 3)
+	assert.EqualError(t, err, "fn params num is 2, but got 3")
+
+	res, err := CallFunc(addReturnErrorFunc, 1, 2)
+	assert.EqualError(t, err, "add error")
+	assert.Equal(t, 1, len(res))
+	assert.Equal(t, 0, res[0].Interface())
+
+	res, err = CallFunc(onlyReturnErrorFunc)
+	assert.EqualError(t, err, "error")
+	assert.Equal(t, 0, len(res))
+
+	res, err = CallFunc(emptyFunc)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 0, len(res))
+
+	res, err = CallFunc(addFunc, 1, 2)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 3, res[0].Interface())
+
+	res, err = CallFunc(interfaceParamFunc, 1, 2)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 3, res[0].Interface())
+
+	res, err = CallFunc(sliceParamFunc, []int{1, 2})
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 3, res[0].Interface())
+
+	m := make(map[string]string)
+	res, err = CallFunc(changeMapFunc, m)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, len(m))
+
+	var p int
+	res, err = CallFunc(pointFunc, &p)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, p)
+
+	res, err = CallFunc(pureVariadicFunc)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 0, res[0].Interface())
+
+	// res, err = CallFuncSlice(pureVariadicFunc)
+	// assert.Equal(t, nil, err)
+	// assert.Equal(t, 0, res[0].Interface())
+
+	vp1 := 1
+	vp2 := 2
+	vp3 := 3
+	res, err = CallFunc(pureVariadicFunc, &vp1, &vp2)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 3, res[0].Interface())
+
+	res, err = CallFunc(pureVariadicFunc, &vp1, &vp2, &vp3)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 6, res[0].Interface())
+
+	res, err = CallFunc(variadicFunc)
+	assert.EqualError(t, err, "fn params num is 1 at least, but got 0")
+
+	res, err = CallFunc(variadicFunc, &vp1, &vp2, &vp3)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 6, res[0].Interface())
+
+	res, err = CallFunc(variadicFunc, &vp1)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, res[0].Interface())
+
+	res, err = CallFunc(variadicFunc, &vp1, nil, nil)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, res[0].Interface())
+
+	res, err = CallFunc(variadicFunc, nil, nil, nil)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 0, res[0].Interface())
+
+	res, err = CallFuncSlice(pureVariadicFunc)
+	assert.EqualError(t, err, "use reflect.CallSlice, fn params num should be 1, but got 0")
+
+	res, err = CallFuncSlice(variadicFunc, &vp1, []*int{nil, nil, nil})
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, res[0].Interface())
+
+	res, err = CallFuncSlice(variadicFunc, &vp1, []*int{&vp2, &vp3})
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 6, res[0].Interface())
+
+}
