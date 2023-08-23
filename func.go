8793da2ccbfbe2b40c69ed11b2bb6221f1fef45d
morrisxyang <morris.x.yang@gmail.com>
2023-08-23 19:19:53 +0800

    feat: add CallMethod check and comments.

--- func.go
+++ func.go
@@ -6,7 +6,9 @@ import (
 	"reflect"
 )
 
-// CallFunc ...
+// CallFunc 通过反射调用函数并返回结果.
+// 支持可变参数, 底层使用 reflect.Value 的 Call 方法, 可变参数需拉平, 否则需使用 CallFuncSlice.
+// 无需从返回值 []reflect.Value 中解析错误, 如果被调用函数最后一个返回值返回错误, 错误将被提取出来作为 CallFunc 的最后一个返回值返回.
 func CallFunc(fn interface{}, args ...interface{}) ([]reflect.Value, error) {
 	if fn == nil {
 		return nil, errors.New("fn must not be nil")
@@ -27,10 +29,10 @@ func CallFunc(fn interface{}, args ...interface{}) ([]reflect.Value, error) {
 	}
 
 	reflectArgs := make([]reflect.Value, len(args))
-	// var sliceType reflect.Type
 	for i, arg := range args {
 		// If the argument is nil, use zero value
 		if arg == nil {
+			// 处理可变参数, 可变参数的类型是 Slice, 这里需要创建其元素类型的零值.
 			if typ.IsVariadic() && i >= typ.NumIn()-1 {
 				reflectArgs[i] = reflect.New(typ.In(typ.NumIn() - 1).Elem()).Elem()
 			} else {
@@ -45,7 +47,7 @@ func CallFunc(fn interface{}, args ...interface{}) ([]reflect.Value, error) {
 	retValues = val.Call(reflectArgs)
 
 	if len(retValues) > 0 {
-		// 如果函数最后一个返回值为 error 且不为空, 提取
+		// 如果函数最后一个返回值为 error 且不为空, 提取出来
 		if errResult := retValues[len(retValues)-1].Interface(); errResult != nil {
 			if err, ok := errResult.(error); ok {
 				return retValues[0 : len(retValues)-1], err
@@ -56,7 +58,10 @@ func CallFunc(fn interface{}, args ...interface{}) ([]reflect.Value, error) {
 	return retValues, nil
 }
 
-// CallFuncSlice ...
+// CallFuncSlice 功能同 CallFunc 一致, 支持可变参数, 但底层使用 reflect.Value 的 CallSlice 方法.
+// CallSlice calls the variadic function v with the input arguments in,
+// assigning the slice in[len(in)-1] to v's final variadic argument.
+// For example, if len(in) == 3, v.CallSlice(in) represents the Go call v(in[0], in[1], in[2]...).
 func CallFuncSlice(fn interface{}, args ...interface{}) ([]reflect.Value, error) {
 	if fn == nil {
 		return nil, errors.New("fn must not be nil")
@@ -77,7 +82,6 @@ func CallFuncSlice(fn interface{}, args ...interface{}) ([]reflect.Value, error)
 	}
 
 	reflectArgs := make([]reflect.Value, len(args))
-	// var sliceType reflect.Type
 	for i, arg := range args {
 		// If the argument is nil, use zero value
 		if arg == nil {
@@ -102,10 +106,17 @@ func CallFuncSlice(fn interface{}, args ...interface{}) ([]reflect.Value, error)
 	return retValues, nil
 }
 
-// CallMethod ...
+// CallMethod 调用 obj 对象的 method 方法并返回结果, 支持可变参数.
+// obj 对象必须是结构体或结构体指针.
+// 底层使用 CallFunc, 详见 CallFunc.
 func CallMethod(obj interface{}, method string, params ...interface{}) ([]reflect.Value, error) {
 	if obj == nil {
 		return nil, errors.New("obj must not be nil")
+	}
+
+	typ := Type(obj)
+	if !isSupportedKind(typ.Kind(), []reflect.Kind{reflect.Struct}) {
+		return nil, errors.New("obj must be struct or struct pointer")
 	}
 
 	val := reflect.ValueOf(obj)
@@ -117,10 +128,15 @@ func CallMethod(obj interface{}, method string, params ...interface{}) ([]reflec
 	return CallFunc(methodValue.Interface(), params...)
 }
 
-// CallMethodSlice ...
+// CallMethodSlice 作用同 CallMethod, 底层使用 CallFuncSlice, 详见 CallFuncSlice.
 func CallMethodSlice(obj interface{}, method string, params ...interface{}) ([]reflect.Value, error) {
 	if obj == nil {
 		return nil, errors.New("obj must not be nil")
+	}
+
+	typ := Type(obj)
+	if !isSupportedKind(typ.Kind(), []reflect.Kind{reflect.Struct}) {
+		return nil, errors.New("obj must be struct or struct pointer")
 	}
 
 	val := reflect.ValueOf(obj)
